589c28d07cafcf11089278ee26316ba29c158cf1
il720506 <il720506@gmail.com>
2023-03-29 15:59:38 +0300

    Make func my_translit and edit input test input_tex()

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from transliterate import translit
 
 
 def input_text() -> str:
-    return input("'ж-zh | ё-e | ф-f | ы-y | ю-ju | ю-ju | я-ja | й-j | ш-sh | щ-sch | х-h | э-e |"
-                 " ч-ch | ь-' | ъ-' |'\n--> ")
+    return input("ж-zh | ё-e | ф-f | ы-y | ю-ju | ю-ju | я-ja | й-j | ш-sh | щ-sch | х-h | э-e |"
+                 " ч-ch | ь-'\nType your text tnd press 'Enter'\n--> ")
 
 
-def my_translit(in_text: str) -> str:
-    pass
+def my_translit(in_txt: str) -> str:
+    return translit(in_txt, 'ru')
 
 
 if __name__ == '__main__':
     in_text = input_text()
-    print(in_text)
+    print(my_translit(in_text))
     # ttt = 'жёфыююяйшщхэчьъ'
     # for _ in ttt:
     #     print(f'{_}-{translit(_, "ru", reversed=True)}', end=' | ')
