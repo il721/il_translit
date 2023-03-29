8079cda10f3856105f2bcaa0ae8899c53b36afa0
il720506 <il720506@gmail.com>
2023-03-29 17:02:17 +0300

    Some text edit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyperclip as pc
 
 def input_text() -> str:
     return input("ж-zh | ё-e | ф-f | ы-y | ю-ju | ю-ju | я-ja | й-j | ш-sh | щ-sch | х-h | э-e |"
-                 " ч-ch | ь-'\nType your text tnd press 'Enter':\n ")
+                 " ч-ch | ь-'\nType your text end press 'Enter':\n ")
 
 
 def my_translit(in_txt: str) -> str:
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         in_text = input_text()
         out_text = my_translit(in_text)
         pc.copy(out_text)
-        print(out_text)
+        print(f'Text "{out_text}" copied to clipboard')
         cont_exit = input("Press 'Q' to exit, any key to continue\n").lower()
         if cont_exit == 'q':
             break
