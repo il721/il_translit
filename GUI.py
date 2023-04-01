9f38d4adff44b284acebd1b477f3f3a2c93a3a0f
il720506 <il720506@gmail.com>
2023-04-01 16:53:02 +0300

    Change text

--- GUI.py
+++ GUI.py
@@ -1,18 +1,20 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')
+sg.theme('DarkGrey11')
 # Устанавливаем цвет внутри окна
-layout = [[sg.Text('Некоторый текст в строке №1')],
-          [sg.Text('Введите «хоть что-нибудь» в строку №2'), sg.InputText()],
-          [sg.Button('Ввод'), sg.Button('Отмена')]]
+layout = [[sg.Text(" ж-zh  ё-joo  ф-f  ы-y  ю-ju  ю-ju  я-ja "
+                   " й-j  ш-sh  щ-sch  х-h  ч-ch  ь-'  ъ-~ ")],
+          [sg.Text('Input Text'), sg.InputText()],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
 
+# sg.theme_previewer()
 # Создаем окно
 window = sg.Window('Название окна', layout)
 # Цикл для обработки "событий" и получения "значений" входных данных
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Отмена':
-        # если пользователь закрыл окно или нажал «Отмена»
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        # если пользователь закрыл окно или нажал «Cancel»
         break
     print('Молодец, ты справился с вводом', values[0])
 
