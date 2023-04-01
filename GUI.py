cd9bdbe64a29772dbfb57b808cdf75fecd32efca
il720506 <il720506@gmail.com>
2023-04-01 16:33:47 +0300

    Start GUI with some example of PySimpleGUI

--- GUI.py
+++ GUI.py
@@ -1,1 +1,19 @@
+import PySimpleGUI as sg
 
+sg.theme('DarkAmber')
+# Устанавливаем цвет внутри окна
+layout = [[sg.Text('Некоторый текст в строке №1')],
+          [sg.Text('Введите «хоть что-нибудь» в строку №2'), sg.InputText()],
+          [sg.Button('Ввод'), sg.Button('Отмена')]]
+
+# Создаем окно
+window = sg.Window('Название окна', layout)
+# Цикл для обработки "событий" и получения "значений" входных данных
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Отмена':
+        # если пользователь закрыл окно или нажал «Отмена»
+        break
+    print('Молодец, ты справился с вводом', values[0])
+
+window.close()
