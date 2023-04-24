c770487854fccf177c320e9cde890f609c53e2bb
il720506 <il720506@gmail.com>
2023-04-24 16:22:14 +0300

    Add class TranslatePySide

--- GUI_PySide6.py
+++ GUI_PySide6.py
@@ -1,3 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_Il_Translit import Ui_MainWindow
+
+
+class TranslatePySide(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    pass
